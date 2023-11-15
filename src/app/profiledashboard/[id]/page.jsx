e13bf0a5f0ce13bcb787131d26163b380ce4ef3f
Kartik Patekar <patekar.kartik@coea.ac.in>
2023-11-15 13:04:13 +0530

    ADDED MIDDLEWARE

--- src/app/profiledashboard/[id]/page.jsx
+++ src/app/profiledashboard/[id]/page.jsx
@@ -1,39 +1,125 @@
-'use client'
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-
-export default function Profilepage() {
-
-
+export default function Profilepage({ params }) {
   const router = useRouter();
+  const [responsedata, setResponsedata] = useState(null);
 
   const handlelogout = async () => {
     try {
-      const response = await fetch('/api/users/logout' ,{method:"GET"});
-     
-if(response.status === 200)
-{
-router.push('/LOGIN')
-console.log("Success Logout !");
-}
-else
-{
+      const response = await fetch('/api/users/logout', { method: 'GET' });
 
-  console.log("BAD RESPONSE logout ROUTE !");
-}
-
-
-      
+      if (response.status === 200) {
+        router.push('/LOGIN');
+        console.log('Success Logout!');
+      } else {
+        console.log('BAD RESPONSE logout ROUTE!');
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
-  return (
-    <>
-      <button onClick={handlelogout}>LOGOUT</button>
-    </>
-  );
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/users/me', { method: 'GET' });
+        const data = await response.json();
+        console.log(data);
+        setResponsedata(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        background: '#41b883', // MongoDB Atlas green
+        fontFamily: 'Roboto, sans-serif',
+        color: '#fff',
+      }}
+    >
+      <div
+        style={{
+          textAlign: 'center',
+          width: '60%',
+          padding: '30px',
+          borderRadius: '12px',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+          background: '#ffffff', // MongoDB Atlas white
+          marginBottom: '20px',
+        }}
+      >
+        <h1
+          style={{
+            marginBottom: '20px',
+            fontSize: '32px',
+            fontWeight: 'bold',
+            color: '#41b883', // MongoDB Atlas green
+          }}
+        >
+          WELCOME, {responsedata?.[0]?.username || 'User'}
+        </h1>
+        <p
+          style={{
+            fontWeight: 'bold',
+            marginBottom: '10px',
+            fontSize: '20px',
+            color: '#41b883',
+          }}
+        >
+          ID: {responsedata?.[0]?._id || 'N/A'}
+        </p>
+        <p
+          style={{
+            fontWeight: 'bold',
+            marginBottom: '10px',
+            fontSize: '20px',
+            color: '#41b883',
+          }}
+        >
+          Email: {responsedata?.[0]?.email || 'N/A'}
+        </p>
+        <p
+          style={{
+            fontWeight: 'bold',
+            marginBottom: '10px',
+            fontSize: '20px',
+            color: '#41b883',
+          }}
+        >
+          Number: {responsedata?.[0]?.number || 'N/A'}
+        </p>
+      </div>
+      <button
+        style={{
+          padding: '15px',
+          fontSize: '18px',
+          background: '#3f6a8e', // MongoDB Atlas blue
+          color: '#fff',
+          border: 'none',
+          borderRadius: '8px',
+          cursor: 'pointer',
+        }}
+        onClick={handlelogout}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
+  
+  
+  
+  
+  
+}
---
+++ src/middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+  const { pathname } = request.nextUrl;
+  const token = request.cookies.get('token');
+
+  // List of public paths
+  const publicPaths = ['/', '/LOGIN', '/verifyemail'];
+
+  // Check if the current path is public
+  const isPublicPath = publicPaths.includes(pathname);
+
+  // If it's not a public path and there's no token, redirect to login
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/LOGIN', request.nextUrl));
+  }
+
+  // If it's a public path and there is a token, redirect to home
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL('/profiledashboard', request.nextUrl));
+  }
+
+  // Otherwise, allow access
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/', '/LOGIN', '/verifyemail', '/profiledashboard/:path*'],
+};
