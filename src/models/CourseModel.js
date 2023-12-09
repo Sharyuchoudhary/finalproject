0dad967b9c54e9f5ede9db6af809def92f57d01d
Kartik Patekar <99249006+kichu27@users.noreply.github.com>
2023-12-09 17:48:40 +0530

    Update CourseModel.js

--- src/models/CourseModel.js
+++ src/models/CourseModel.js
@@ -38,6 +38,6 @@ const courseSchema = new mongoose.Schema({
 });
 
 // Create the Course model
-export default const Course = mongoose.models.Course || mongoose.model('Course', courseSchema);
+export const Course = mongoose.models.Course || mongoose.model('Course', courseSchema);
 
 
