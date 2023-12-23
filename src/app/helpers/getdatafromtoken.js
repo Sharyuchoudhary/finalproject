4c9df541ce6e61a059f1b784cf83903ef5ad5c4e
Kartik Patekar <patekar.kartik@coea.ac.in>
2023-12-24 00:02:51 +0530

    a14

--- src/app/helpers/getdatafromtoken.js
+++ src/app/helpers/getdatafromtoken.js
@@ -6,7 +6,12 @@ export default async function getdatafromtoken (request)
     try {
         
         const token = request.cookies.get('token')?.value || '' ; 
+        if(!token)
+        {
+          return NextResponse.json({"msg" : "UNSUCKTOKEN"})
 
+        }
+        
         const decodedtoken = jwt.verify(token , 'finalproject')
         console.log(decodedtoken.id);
         return decodedtoken.id ;
