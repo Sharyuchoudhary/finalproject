cb6516c7db4c425253b42038de8bed2d8b51f306
Kartik Patekar <patekar.kartik@coea.ac.in>
2023-12-23 14:04:55 +0530

    a8

--- src/app/api/users/me/route.js
+++ src/app/api/users/me/route.js
@@ -11,7 +11,8 @@ export async function GET(request)
         await Connect()
         const userid = await getdatafromtoken(request)
         const user = await User.find({_id:userid}).select(("-password")) ;
-        return NextResponse.json({user:user , name:user[0].username})
+        const name = await user[0].username
+        return NextResponse.json({user:user , name:name})
         
     } catch (error) {
        console.log(error); 
