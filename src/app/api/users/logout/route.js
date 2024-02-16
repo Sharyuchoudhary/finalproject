9f2ea5b548daeebfeda50b66d6556a8b58c45b0e
Kartik Patekar <patekar.kartik@coea.ac.in>
2024-02-16 12:21:11 +0530

    update cookies function

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -1,11 +1,13 @@
 
 import { NextResponse } from "next/server";
+import { cookies } from 'next/headers'
 
 export async function GET() {
   try {
-       
+ 
    const response =  NextResponse.json({message:"LOGOUT SUCCESS !" , success:true , }) ; 
-   response.cookies.set('token' , "" , {httpOnly:true , expires: new Date(0)})
+   cookies().delete('token')
+  //  response.cookies.set('token' , "" , {httpOnly:true , expires: new Date(0)})
 
     return response ; 
   } catch (error) {
